5398cc86c336d849cb00db574e4ef6991fadd63a
ye1002 <C112181126@nkust.edu.tw>
2025-10-30 11:42:11 +0800

    C
    Added HTML output and a weekday array with a loop.

--- 103004.php
+++ 103004.php
@@ -1,10 +1,17 @@
-<?php 
-$arr["color"] = "rad";  
-$arr["name"] = "jack";
-$arr["shape"] = "t";
-
-foreach ($arr as $key => $value) {
-echo "$k~$v~!<hr/>";
+<h1>sid:C112181126
+<br>
+name:jack</h1>
+<p></p>
+<?php
+$weekday = array( 
+            1=>"Mon", 
+                "Tue", 
+                "Wed", 
+                "Thu",
+                "Fri", 
+                "Sat", 
+                "Sun"
+                );
+for ( $i = 1; $i <= count($weekday); $i++ ){
+    echo "$i : $weekday[$i] ";
 }
-echo"<br>";
-print_r($arr); 
